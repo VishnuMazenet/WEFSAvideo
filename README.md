77421f68871b3744321b7f13d0da535e4a6014f1
VishnuMazenet <jr_developer2@mazenetsolution.com>
2023-08-16 18:49:00 +0530

    updated readme.md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# WEFSAvideo
-To get the name of the viewer and to show the view of that video in the page. views and viewer name will saved in the database.
+The code you have provided is a simple web application that allows users to view a video and see the number of views it has. The application also has a page for admins to manage the viewers.
+
+The index.php file is the main page of the application. It loads the video and displays the number of views. It also has a form for users to enter their name. If the user is an admin, they will be redirected to the view.php page.
+
+The view.php file is the admin page. It displays a list of all the viewers and allows admins to delete them. It also has a button to delete all the views.
+
+The delete_views.php and delete_viewers.php files are used to delete the views and viewers from the database.
+
+The config.php file contains the database connection details.
+
+The bootstrap.min.css and jquery-3.6.4.min.js files are required for the styling and functionality of the application.
+
+Here are the steps on how to run the code:
+
+1. Download the code from GitHub.
+2. Create a new database and import the wefsa.sql file.
+3. Open the index.php file in a text editor and change the database connection details to match your own database.
+4. Upload the files to a web server.
+5. Open the index.php file in a browser.
+
+You can now view the video and see the number of views. If you are an admin, you can also manage the viewers.
--- index.php
+++ index.php
@@ -7,14 +7,14 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-  $row = $result->fetch_assoc();
-  $views = $row["views"];
+    $row = $result->fetch_assoc();
+    $views = $row["views"];
 } else {
-  $views = 0;
-  $stmt = $conn->prepare("INSERT INTO views (views) VALUE (?)");
-  $stmt->bind_param("i", $views);
-  $stmt->execute();
-  $result = $stmt->get_result();
+    $views = 0;
+    $stmt = $conn->prepare("INSERT INTO views (views) VALUE (?)");
+    $stmt->bind_param("i", $views);
+    $stmt->execute();
+    $result = $stmt->get_result();
 }
 
 $stmt->close();
@@ -26,85 +26,85 @@ $conn->close();
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>WEFSA</title>
-  <link rel="stylesheet" href="bootstrap.min.css">
-  <script src="jquery-3.6.4.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEFSA</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <script src="jquery-3.6.4.min.js"></script>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="https://wefsa.com/">WEFSA</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="https://wefsa.com/">WEFSA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarColor03">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <form class="form-inline my-2 my-lg-0">
-          <label>WELCOME... </label>
-          <label id='name'></label>
-        </form>
+        <div class="collapse navbar-collapse" id="navbarColor03">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <form class="form-inline my-2 my-lg-0">
+                    <label>WELCOME... </label>
+                    <label id='name'></label>
+                </form>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <p class="my-3">Views: <?php echo $views; ?></p>
+                <video width="100%" controls>
+                    <source src="video.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
     </div>
-  </nav>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-8 offset-md-2">
-        <p class="my-3">Views: <?php echo $views; ?></p>
-        <video width="100%" controls>
-          <source src="video.mp4" type="video/mp4">
-        </video>
-      </div>
-    </div>
-  </div>
-  <script>
-    $(document).ready(function() {
-      let name = prompt("Please enter your name:");
-      while (name === "" || name === null) {
-        name = prompt("Please enter your name to continue watching the video:");
-      }
-      document.getElementById('name').innerHTML = name;
-      if (name !== null && name !== "") {
-        $.ajax({
-          url: "admin_check.php",
-          method: "POST",
-          data: {
-            name: name
-          },
-          success: function(response) {
-            if (response == "match") {
-              window.location.href = "view.php";
-            } else {
-              $.ajax({
-                url: "save.php",
-                method: "POST",
-                data: {
-                  name: name,
-                  views: '<?php echo $views; ?>'
-                },
-                success: function(response) {
-                  console.log(response);
-                  console.log("Name updated");
-                },
-                error: function() {
-                  console.log(response);
-                  console.log("error");
-                }
-              });
+    <script>
+        $(document).ready(function() {
+            let name = prompt("Please enter your name:");
+            while (name === "" || name === null) {
+                name = prompt("Please enter your name to continue watching the video:");
             }
-          }
-        });
+            document.getElementById('name').innerHTML = name;
+            if (name !== null && name !== "") {
+                $.ajax({
+                    url: "admin_check.php",
+                    method: "POST",
+                    data: {
+                        name: name
+                    },
+                    success: function(response) {
+                        if (response == "match") {
+                            window.location.href = "view.php";
+                        } else {
+                            $.ajax({
+                                url: "save.php",
+                                method: "POST",
+                                data: {
+                                    name: name,
+                                    views: '<?php echo $views; ?>'
+                                },
+                                success: function(response) {
+                                    console.log(response);
+                                    console.log("Name updated");
+                                },
+                                error: function() {
+                                    console.log(response);
+                                    console.log("error");
+                                }
+                            });
+                        }
+                    }
+                });
 
-      }
-    });
-  </script>
+            }
+        });
+    </script>
 
 </body>
 
